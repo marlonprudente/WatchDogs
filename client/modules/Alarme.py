d870a8adccc077291d6a21d52198d2c58b68769f
Marlon Prudente <marlonoliveira@alunos.utfpr.edu.br>
2017-06-10 12:12:57 -0300

    alteracao

--- client/modules/Alarme.py
+++ client/modules/Alarme.py
@@ -9,22 +9,24 @@ PRIORITY = 20
 def handle(text, mic, profile):
    try:		        
 
-	cmd = 'sudo python ARQUIVO SENSORES '
+	cmd = 'sudo python /home/py/Sensores.py'
 	
   	if bool(re.search(r'\b(LIGAR|\sLIGAR\s.*alarme)\b', text, re.IGNORECASE)):
 		mic.say('Alarmes Ligados')
 		os.system(cmd+"switch on")
-    	elif bool(re.search(r'\b(DESLIGAR?|\sOFF?\s.*desligar)\b', text, re.IGNORECASE)):
-    	 	mic.say('Alarmes Desligados')
-       		os.system(cmd+"switch off")
-		elif bool(re.search(r'\b(DELAY?|\sDELAY?\s.*delay)\b', text, re.IGNORECASE)):
-			time.sleep(600)
-			mic.say('Alarmes Ligados')
-			os.system(cmd+"switch on")
-    	else:
+
+    elif bool(re.search(r'\b(DESLIGAR?|\sOFF?\s.*desligar)\b', text, re.IGNORECASE)):
+    	 mic.say('Alarmes Desligados')
+       	os.system(cmd+"switch off")
+
+	elif bool(re.search(r'\b(DELAY?|\sDELAY?\s.*delay)\b', text, re.IGNORECASE)):
+		time.sleep(600)
+		mic.say('Alarmes Ligados')
+		os.system(cmd+"switch on")
+		
+    else:
 		mic.say('light now is blinking 5 times')
 		os.system(cmd+"blink 5 1")
-
     	mic.say('DONE... ')
    
    except:
